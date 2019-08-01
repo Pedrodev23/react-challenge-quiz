37e8c634730e426144474ecd53efe0c7e745fb8a
Pedrodev23 <dev.pedro.23@gmail.com>
2019-08-01 09:08:58 -0300

    Add the shuffleArrayHelper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,3 +1,7 @@
 export const uriDecodeHelper = function(encodedURI) {
     return encodedURI ? decodeURIComponent(encodedURI) : ''
 }
+
+export const shuffleArrayHelper = function(array) {
+    return array.sort(() => Math.random() - 0.5);
+}
