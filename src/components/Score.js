3145221cff01148cb3e8cb2a50d009f3e738dcd5
Pedrodev23 <dev.pedro.23@gmail.com>
2019-08-01 10:26:02 -0300

    Add the ScoreBar

---
+++ src/components/Score.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Score = styled.span((props) => (`
+    color: #494949;
+    font-size: 14px;
+    fload: ${ props.float };
+`))
+
+const ScoreBar = (props) => (
+    <div>
+        
+    </div>
+)
+export default TitleBar
