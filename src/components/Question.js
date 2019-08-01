22fce7d06e5c272280333db2bca706f94387bab4
Pedrodev23 <dev.pedro.23@gmail.com>
2019-08-01 09:40:43 -0300

    Add the displaying the answers

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,17 +1,74 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import { uriDecodeHelper, shuffleArrayHelper, combineArrayHelper } from '../helpers'
 
 const Content = styled.div`
         padding: 10px;
     `
+const Answers = styled.div`
+    padding: 10px;
+`
 
-export default function(props) {
-    const { question } = props
+const Answer = styled.button((props) => (
+    `${props.value == props.answer ?
+        {
+            color: '#FFFFFF',
+            backgroundColor: '#000000'
+        }
+        :
+        {
+            color: '#FFFFFF',
+            backgroundColor: '#000000'
+        }
+    }`
+))
+
+export default function({ 
+    question, 
+    correct_answer, 
+    incorerect_answers,
+}) {
+    const [ correctAnswer, setCorrectAnswer ] = useState()
+    const [ mixedAnswers, setMixedAnswers ] = useState([])
+    const [ answer, setAnswer ] = useState(null)
+
+    useEffect(() => {
+        if( !correctAnswer || !incorerect_answers ) return
+
+        setCorrectAnswer( correct_answer )
+
+        let mixedArray = shuffleArrayHelper(
+            combineArrayHelper( incorerect_answers, correct_answer )
+        )
+
+        setMixedAnswers( mixedArray )
+    }, [
+        correct_answer, 
+        incorerect_answers
+    ])
+
+    const handleAnswer = (answer) => {
+        setAnswer( answer )
+        if ( correctAnswer == answer ) {
+
+        }
+    }
+
     return (
         <div>
             <Content>
-                { question }
+                { uriDecodeHelper( question ) }
             </Content>
+            <Answers >
+                { 
+                    mixedAnswers.map((key, value) => (
+                        <Answer 
+                            key={key}
+                            answer={answer} 
+                            onClick={handleAnswer(value)}/>
+                    ))
+                }
+            </Answers>
         </div>
     )
 } 
